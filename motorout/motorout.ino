e97cd74aa5d714df8bd47001a62d5da4af310786
Luca Scheuer <scheuer.luca@gmail.com>
2014-02-28 12:25:39 -0800

    Second motor working

--- motorout/motorout.ino
+++ motorout/motorout.ino
@@ -14,14 +14,17 @@ void loop() {
 void setupMotor() {
   //http://www.avrbeginners.net/architecture/timers/timers.html#16bit_timer
   pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
   TCCR1A |= (1<<WGM11);  //Setting Timer comparator 1A register bit 1 (WGM11) to on
   TCCR1A &= ~(1<<WGM10); //Setting Timer comparator 1A register bit 0 (WGM10) to off
   TCCR1B &= ~(1<<WGM12) &  ~(1<<CS11) & ~(1<<CS12); //Turn off WGM12, CS11, CS12, 
   TCCR1B |= (1<<WGM13) | (1<<CS10); //Turn on WGM13 and CS10
- 
   ICR1 |= 0x3FFF;        //Setting Input Capture register to high
-  TCCR1A |= (1<<COM1A1); //Pin 9, Setting Timer comparator 1A register bit 7 (WGM11) to on
+  TCCR1A |= (1<<COM1A1); //Pin 9 connected to comparator 1A
+  TCCR1A |= (1<<COM1B1); //Pin 10 connected to comparator 1B
   OCR1A |= 1200<<3;     //Setting Timer comparator output register to zero percent throttle.
+  OCR1B |= 1200<<3;
+  // Second Timer
   delay(500);
 }
 
@@ -29,6 +32,7 @@ void setMotor() {
   counter++;
   delay(500);
   OCR1A = motorVal;
+  OCR1B = motorVal;
   if (counter > 10) {
     if (motorVal < (1600<<3)) {
       motorVal = (1600<<3);
